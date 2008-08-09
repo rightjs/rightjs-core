1d54ca16b88de914c5c5c95d94842208a671724d
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2008-08-10 01:00:41 +0400

    implemented some array methods

--- src/array.js
+++ src/array.js
@@ -55,6 +55,80 @@ Object.extend(Array.prototype, {
   },
   
   /**
+   * returns the first element of the array
+   *
+   * @return mixed first element of the array
+   */
+  first: function() {
+    return this[0];
+  },
+  
+  /**
+   * returns the last element of the array
+   *
+   * @return mixed last element of the array
+   */
+  last: function() {
+    return this[this.length-1];
+  },
+  
+  /**
+   * returns the array size
+   *
+   * @return Integer the array size
+   */
+  size: function() {
+    return this.length;
+  },
+  
+  /**
+   * merges all the arrays passed as the arguments
+   *
+   * @return Array this
+   */
+  merge: function() {
+    for (var i=0; i < arguments.length; i++) {
+      if (defined(arguments[i]['length'])) {
+        for (var j=0; j < arguments[i].length; j++) {
+          this.push(arguments[i][j]);
+        }
+      } else {
+        this.push(arguments[i]);
+      }
+    }
+  },
+  
+  /**
+   * returns a copy of the array whithout any null or undefined values
+   *
+   * @return Array filtered version
+   */
+  compact: function() {
+    var copy = [];
+    for (var i=0; i < this.length; i++) {
+      if (this[i] != null && this[i] !== undefined) {
+        copy.push(this[i]);
+      }
+    }
+    return copy;
+  },
+  
+  /**
+   * returns a copy of the array which contains only the unique values
+   *
+   * @return Array filtered copy
+   */
+  uniq: function() {
+    var copy = [];
+    for (var i=0; i < this.length; i++) {
+      if (!copy.includes(this[i])) {
+        copy.push(this[i]);
+      }
+    }
+    return copy;
+  },
+  
+  /**
    * checks if all of the given values
    * exists in the given array
    *
@@ -69,5 +143,22 @@ Object.extend(Array.prototype, {
       }
     }
     return true;
+  },
+  
+  /**
+   * returns a copy of the array without the items passed as the arguments
+   *
+   * @param mixed value
+   * ......
+   * @return Array filtered copy
+   */
+  without: function() {
+    var filter = $A(arguments), copy = [];
+    for (var i=0; i < this.length; i++) {
+      if (!filter.includes(this[i])) {
+        copy.push(this[i]);
+      }
+    }
+    return copy;
   }
 });
--- test/unit/array_test.js
+++ test/unit/array_test.js
@@ -24,6 +24,29 @@ var ArrayTest = TestCase.create({
     }));
   },
   
+  testFirst: function() {
+    this.assertEqual(1, [1,2,3].first());
+    this.assert([].first() === undefined);
+  },
+  
+  testLast: function() {
+    this.assertEqual(4, [1,2,3,4].last());
+    this.assert([].last() === undefined);
+  },
+  
+  testSize: function() {
+    this.assertEqual(2, [1,2].size());
+    this.assertEqual(4, [1,2,3,4].size());
+  },
+  
+  testCompact: function() {
+    this.assertEqual([1,2,3,4], [1,null, null,2,undefined,3,4].compact());
+  },
+  
+  testUnique: function() {
+    this.assertEqual([1,2,3,4], [1,2,1,2,3,1,2,3,4,1,2,3,4].uniq());
+  },
+  
   testIncludes: function() {
     this.assert([true].includes(true));
     this.assert([1,2,3,4,5].includes(2,4));
@@ -31,5 +54,9 @@ var ArrayTest = TestCase.create({
     this.assertFalse([true].includes(false));
     this.assertFalse([1,2,3,4,5].includes(6));
     this.assertFalse([1,2,3,4,5].includes(2, 6));
+  },
+  
+  testWithout: function() {
+    this.assertEqual([1,4], [1,2,3,4].without(2,3));
   }
 });
