1855429ae3d8a3736a622de960315a171fbd5215
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2008-08-11 00:31:56 +0400

    implemented the basic strings extention

--- src/string.js
+++ src/string.js
@@ -3,3 +3,152 @@
  *
  * Copyright (C) 2008 Nikolay V. Nemshilov aka St. <nemshilov#gma-il>
  */
+Object.extend(String, {
+  // this is for unicode regexps 
+  UTF8_DOWNS: 'a-zàèìòùáéíóúýâêîôûãñõäëïöü¡¿çßøåæþðёйцукенгшщзхъфывапролджэячсмитьбю',
+  UTF8_UPS: 'A-ZÀÈÌÒÙÁÉÍÓÚÝÂÊÎÔÛÃÑÕÄËÏÖÜ¡¿ÇØÅÆÞÐЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+});
+Object.extend(String.prototype, {
+  /**
+   * checks if the string is an empty string
+   *
+   * @return boolean check result
+   */
+  empty: function() {
+    return this == '';
+  },
+  
+  /**
+   * checks if the string contains only white-spaces
+   *
+   * @return boolean check result
+   */
+  blank: function() {
+    return /^\s*$/.test(this);
+  },
+  
+  /**
+   * removes trailing whitespaces
+   *
+   * @return String trimmed version
+   */
+  trim: function() {
+    return this.replace(/^\s+|\s+$/g, '');
+  },
+  
+  /**
+   * returns a copy of the string with all the tags removed
+   * @return String without tags
+   */
+  stripTags: function() {
+    return this.replace(/<\/?[^>]+>/ig, '');
+  },
+  
+  /**
+   * removes all the scripts declarations out of the string
+   * @param mixed option. If it equals true the scrips will be executed, 
+   *                      if a function the scripts will be passed in it
+   * @return String without scripts
+   */
+  stripScripts: function(option) {
+    var scripts = '';
+    var text = this.replace(/<script[^>]*>([\s\S]*?)<\/script>/img, function(match, source) {
+      scripts += source.trim() + "\n";
+      return '';
+    });
+    
+    if (option === true) { eval(scripts);
+    } else if (typeof(option) == 'function') {
+      option(scripts, text);
+    }
+    
+    return text;
+  },
+  
+  /**
+   * converts underscored or dasherized string to a camelized one
+   * @returns String camelized version
+   */
+  camelize: function() {
+    var prefix = this.match(/^(\-|_)+?/g) || ''; // <- keeps start dashes alive
+    return prefix + this.substr(prefix.length, this.length).replace(
+       new RegExp('(\\-|_)+?(\\D)', 'g'), function(match) {
+         return match.replace(/\-|_/, '').toUpperCase();
+      });
+  },
+  
+  /**
+   * converts a camelized or dasherized string into an underscored one
+   * @return String underscored version
+   */
+  underscored: function() {
+    return this.replace(/([a-z0-9])([A-Z]+)/g, function(match, first, second) {
+      return first+"_"+(second.length > 1 ? second : second.toLowerCase());
+    }).replace(/\-/g, '_');
+  },
+
+  /**
+   * returns a capitalised version of the string
+   *
+   * @return String captialised version
+   */
+  capitalize: function() {
+    return this.replace(new RegExp('(^|\\s|\\-|_)['+String.UTF8_DOWNS+']', 'g'), function(match) {
+      return match.toUpperCase();
+    });
+  },
+  
+  /**
+   * checks if the string contains the given substring
+   *
+   * @param String string
+   * @return boolean check result
+   */
+  includes: function(string) {
+    return this.indexOf(string) != -1;
+  },
+  
+  /**
+   * checks if the string starts with the given substring
+   *
+   * @param String string
+   * @param boolean ignore the letters case
+   * @return boolean check result
+   */
+  startsWith: function(string, ignorecase) {
+    var start_str = this.substr(0, string.length);
+    return ignorecase ? start_str.toLowerCase() == string.toLowerCase() : 
+      start_str == string;
+  },
+  
+  /**
+   * checks if the string ends with the given substring
+   *
+   * @param String substring
+   * @param boolean ignore the letters case
+   * @return boolean check result
+   */
+  endsWith: function(string, ignorecase) {
+    var end_str = this.substr(this.length - string.length, this.length);
+    return ignorecase ? end_str.toLowerCase() == string.toLowerCase() :
+      end_str == string;
+  },
+  
+  /**
+   * converts the string to an integer value
+   * @param Integer base
+   * @return Integer or NaN
+   */
+  toInt: function(base) {
+    return parseInt(this, base || 10);
+  },
+  
+  /**
+   * converts the string to a float value
+   * @param boolean flat if the method should not use a flexible matching
+   * @return Float or NaN
+   */
+  toFloat: function(strict) {
+    return parseFloat(strict ? this : this.replace(',', '.').replace(/(\d)-(\d)/g, '$1.$2'));
+  }
+});
--- test/unit/string_test.js
+++ test/unit/string_test.js
@@ -6,7 +6,101 @@
 var StringTest = TestCase.create({
   name: 'StringTest',
   
-  testFoo: function() {
-    this.assert(true);
+  testEmpty: function() {
+    this.assert(''.empty());
+    this.assertFalse(' '.empty());
+  },
+  
+  testBlank: function() {
+    this.assert(''.blank());
+    this.assert(" \t\n ".blank());
+    this.assertFalse(' s'.blank());
+  },
+  
+  testTrim: function() {
+    this.assert('asdf', " \t\nasdf\t\n\r \n".trim());
+    this.assert('фыва', " \t\nфыва\t\n\r \n".trim());
+  },
+  
+  testStripTags: function() {
+    this.assertEqual('asdf фыва asdf', 'asdf <b>фыва</b><br/> <i>asdf</i>'.stripTags());
+  },
+  
+  testStripScripts: function() {
+    this.assertEqual('asdf <i>asdf</i>', "asdf<script>\n \t\nalert('фыва');</script> <i>asdf<script>asdf</script></i>".stripScripts());
+  },
+  
+  testStripScriptsWithScriptsEval: function() {
+    self.______called______ = false;
+    "asdf <script>self.______called______ = true;</script>".stripScripts(true);
+    this.assert(self.______called______);
+    self.______called______ = undefined;
+  },
+  
+  testStripScriptsWithScriptsExtraction: function() {
+    var scripts = '';
+    "<script>\nalert('bla');\n\n\n</script> asdf <script>\talert('foo');</script>".stripScripts(
+      function(source) { scripts = source;
+    });
+    this.assertEqual("alert('bla');\nalert('foo');\n", scripts);
+  },
+  
+  testCamelize: function() {
+    this.assertEqual('asdfAsdfAsdf', 'asdf_asdf_asdf'.camelize());
+    this.assertEqual('asdfAsdfAsdf', 'asdf-asdf-asdf'.camelize());
+    this.assertEqual('_asdfAsdfASDf', '_asdf_asdf_ASDf'.camelize());
+    
+    this.assertEqual('фываФываФыва', 'фыва_фыва_фыва'.camelize());
+    this.assertEqual('фываФываФыва', 'фыва-фыва-фыва'.camelize());
+    this.assertEqual('_фываФываФЫВА', '_фыва_фыва_ФЫВА'.camelize());
+  },
+  
+  testUnderscored: function() {
+    this.assertEqual('asdf_asdf_asdf', 'asdfAsdfAsdf'.underscored());
+    this.assertEqual('asdf_asdf_ASDF', 'asdfAsdfASDF'.underscored());
+    this.assertEqual('asdf_asdf_asdf', 'asdf-asdf-asdf'.underscored());
+  },
+
+  testCapitalize: function() {
+    this.assertEqual('Asdf', 'asdf'.capitalize());
+    this.assertEqual('Asdf Asdf', 'asdf asdf'.capitalize());
+    this.assertEqual('Asdf-Asdf', 'asdf-asdf'.capitalize());
+    this.assertEqual('Мама Мыла Раму', 'мама мыла раму'.capitalize());
+    this.assertEqual('Мама-Мыла Раму', 'мама-мыла раму'.capitalize());
+  },
+  
+  testIncludes: function() {
+    this.assert('asdf sdfg dfg'.includes('sdf'));
+    this.assertFalse('asdf sdfg df'.includes('qwer'));
+  },
+  
+  testStartsWith: function() {
+    this.assert('asdf sdf'.startsWith('asdf'));
+    this.assertFalse('asdf sdf'.startsWith('Asdf'));
+    
+    this.assert('asdf sdf'.startsWith('Asdf', true), 'test checking with ignore case');
+  },
+  
+  testEndsWith: function() {
+    this.assert('asdf qwer'.endsWith('qwer'));
+    this.assertFalse('asdf qwer'.endsWith('Qwer'));
+    
+    this.assert('asdf qwer'.endsWith('Qwer', true), 'test checking with ignore case');
+  },
+  
+  testToInt: function() {
+    this.assertEqual(1, '1'.toInt());
+    this.assertEqual(2, '2.2'.toInt());
+    this.assert(isNaN('asdf'.toInt()));
+  },
+  
+  testToFloat: function() {
+    this.assertEqual(1.0, '1'.toFloat());
+    this.assertEqual(2.2, '2.2'.toFloat());
+    this.assertEqual(3.3, '3,3'.toFloat());
+    this.assertEqual(4.4, '4-4'.toFloat());
+    this.assertEqual(-4.4, '-4-4'.toFloat());
+    this.assertEqual(4.0, '4-4'.toFloat(true));
+    this.assert(isNaN('asdf'.toFloat()));
   }
 });
