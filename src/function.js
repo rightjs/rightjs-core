e2a2c220d43ebe90a240a7dfa9d133fc8f3ab459
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2008-08-11 23:42:21 +0400

    added the Function extending methods sketch

--- src/function.js
+++ src/function.js
@@ -3,3 +3,19 @@
  *
  * Copyright (C) 2008 Nikolay V. Nemshilov aka St. <nemshilov#gma-il>
  */
+Object.extend(Function.prototype, {
+  bind: function(scope, args) {
+  },
+  
+  bindWithEvent: function(scope, args) {
+  },
+  
+  carry: function() {
+  },
+  
+  delay: function(timeout, scope, args) {
+  },
+  
+  periodical: function(interval, scope, args) {
+  }
+});
