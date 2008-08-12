bf8231a45b3a7e12bc17064f2dd58ed8602eae4a
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2008-08-12 11:28:42 +0400

    implemented the function extentions

--- src/function.js
+++ src/function.js
@@ -4,18 +4,72 @@
  * Copyright (C) 2008 Nikolay V. Nemshilov aka St. <nemshilov#gma-il>
  */
 Object.extend(Function.prototype, {
-  bind: function(scope, args) {
+  /**
+   * binds the function to be executed in the given scope
+   *
+   * @param Object scope
+   * @param mixed optional curry (left) argument
+   * ....
+   * @return Function binded function
+   */
+  bind: function() {
+    if (arguments.length < 2 && !defined(arguments[0])) { return this; }
+    
+    var _method = this, args = $A(arguments), scope = args.shift();
+    return function() {
+      return _method.apply(scope, args.concat($A(arguments)));
+    };
   },
   
-  bindWithEvent: function(scope, args) {
+  /**
+   * binds the function as an event listener to the given scope object
+   *
+   * @param Object scope
+   * @param mixed optional curry (left) argument
+   * .......
+   * @return Function binded function
+   */
+  bindAsEventListener: function() {
+    var _method = this, args = $A(arguments), scope = args.shift();
+    return function(event) {
+      return _method.apply(scope, [event || window.event].concat(args).concat($A(arguments)));
+    };
   },
   
-  carry: function() {
+  /**
+   * allows you to put some curry in your cookery
+   *
+   * @param mixed value to curry
+   * ....
+   * @return Function carried function
+   */
+  curry: function() {
+    return this.bind.apply(this, [this].concat($A(arguments)));
   },
   
-  delay: function(timeout, scope, args) {
+  /**
+   * delays the function execution
+   *
+   * @param Integer delay ms
+   * @param mixed value to curry
+   * .....
+   * @return Integer timeout marker
+   */
+  delay: function() {
+    var args = $A(arguments), timeout = args.shift();
+    return window.setTimeout(this.bind.apply(this, [this].concat(args)), timeout);
   },
   
-  periodical: function(interval, scope, args) {
+  /**
+   * creates a periodical execution of the function with the given timeout
+   *
+   * @param Integer delay ms
+   * @param mixed value to curry
+   * ...
+   * @return Ineger interval marker
+   */
+  periodical: function() {
+    var args = $A(arguments); timeout = args.shift();
+    return window.setInterval(this.bind.apply(this, [this].concat(args)), timeout);
   }
 });
--- test/unit/function_test.js
+++ test/unit/function_test.js
@@ -6,7 +6,77 @@
 var FunctionTest = TestCase.create({
   name: 'FunctionTest',
   
-  testFoo: function() {
-    this.assert(true);
+  testBind: function() {
+    var obj = {
+      attr: null
+    };
+    
+    var func = (function() {
+      return this.attr = 'bind';
+    }).bind(obj);
+    
+    this.assertInstanceOf(Function, func);
+    this.assertEqual('bind', func());
+    this.assertEqual('bind', obj.attr);
+  },
+  
+  testBindWithCurry: function() {
+    var obj = {
+      attr: null
+    };
+    
+    var func = (function(def, users) {
+      return this.attr = users || def;
+    }).bind(obj, 'default');
+    
+    this.assertInstanceOf(Function, func);
+    this.assertEqual('default', func());
+    this.assertEqual('default', obj.attr);
+    
+    this.assertEqual('users', func('users'));
+    this.assertEqual('users', obj.attr);
+  },
+  
+  testBindAsEventListener: function() {
+    var obj = {
+      'event': null,
+      'attr': null,
+    };
+    
+    var func = (function(event, def, users) {
+      this.event = event;
+      return this.attr = def || users;
+    }).bindAsEventListener(obj, 'default');
+    
+    this.assertInstanceOf(Function, func);
+    this.assertEqual('default', func('event'));
+    this.assertEqual('default', obj.attr);
+  },
+  
+  testCurry: function() {
+    var func = function(x,y) { return x * y; };
+    var triple = func.curry(3);
+    var quadro = func.curry(4);
+    
+    this.assertInstanceOf(Function, triple);
+    this.assertInstanceOf(Function, quadro);
+    this.assertEqual(15, triple(5));
+    this.assertEqual(20, quadro(5));
+  },
+  
+  testDelay: function() {
+    var timeout = (function(txt) {
+      //alert(txt);
+    }).delay(5000, 'some text');
+    
+    this.assertNotNull(timeout);
+  },
+  
+  testPeriodical: function() {
+    var interval = (function(txt) {
+      //alert(txt);
+    }).periodical(5000, 'some text');
+    
+    this.assertNotNull(interval);
   }
 });
