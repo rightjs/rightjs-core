3dd755812e83dc0c2b4a634704517303a6a01802
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-04-25 16:44:04 +0400

    scripts test initiation update

---
+++ test/inits.js
@@ -0,0 +1,35 @@
+var scripts = [
+  'core/util.js',
+  'core/browser.js',
+  'native/object.js',
+  'native/math.js',
+  'native/array.js',
+  'native/string.js',
+  'native/function.js',
+  'native/number.js',
+  'core/class.js',
+  'core/class/util.js',
+  'core/class/methods.js',
+  'util/cookie.js',
+  'util/break.js',
+  'dom/element.js',
+  'dom/element/commons.js',
+  'dom/element/structs.js',
+  'dom/element/styles.js',
+  'dom/element/events.js',
+  'dom/event.js',
+  'dom/event/base.js',
+  'dom/event/mouse.js',
+  'dom/event/keyboard.js',
+  'dom/selector.js',
+  'dom/selector/atom.js',
+  'dom/selector/manual.js',
+  'dom/selector/native.js',
+  'dom/selector/multiple.js',
+  'dom/extend.js'
+];
+
+for (var i=0; i < scripts.length; i++) {
+  document.writeln('<scr'+'ipt type="text/javascript" src="../src/'+scripts[i]+'"></scr'+'ipt>');
+}
+
