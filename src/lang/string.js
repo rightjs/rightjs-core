f0fcccb096b8d6046d0942fe1bd2b3f02386cba3
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-08-21 22:04:49 +0400

    #73 String UTF-8 and regular expressions reworked

--- src/lang/string.js
+++ src/lang/string.js
@@ -9,13 +9,24 @@
  *
  * Copyright (C) 2008-2009 Nikolay V. Nemshilov aka St. <nemshilov#gma-il>
  */
-$ext(String, {
-  // this is for unicode regexps 
-  UTF8_DOWNS: 'a-zàèìòùáéíóúýâêîôûãñõäëïöü¡¿çßøåæþðёйцукенгшщзхъфывапролджэячсмитьбю',
-  UTF8_UPS: 'A-ZÀÈÌÒÙÁÉÍÓÚÝÂÊÎÔÛÃÑÕÄËÏÖÜ¡¿ÇØÅÆÞÐЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
-});
-
-$ext(String.prototype, {
+$ext(String.prototype, (function() {
+  
+  // predefining the regular expressions to speed the things up
+  
+  var SPACES_ONLY_RE         = /^\s*$/,
+      START_SPACES_RE        = /^\s\s*/,
+      SOME_SPACE_RE          = /\s/,
+      STRIP_TAGS_RE          = /<\/?[^>]+>/ig,
+      STRIP_SCRIPTS_RE       = /<script[^>]*>([\s\S]*?)<\/script>/img,
+      START_DASHES_RE        = /^(\-|_)+?/g,
+      SOME_DASH_RE           = /\-|_/,
+      ALL_DASHES_RE          = /\-/g,
+      DASH_PLUS_UPCASE_RE    = /(\-|_)+?(\D)/g,
+      CASE_CHANGE_RE         = /([a-z0-9])([A-Z]+)/g,
+      LOWCASED_WORD_START_RE = /(^|\s|\-|_)[a-z\u00e0-\u00fe\u0430-\u045f]/g,
+      FLOAT_DIGIT_RE         = /(\d)-(\d)/g;
+  
+return {
   /**
    * checks if the string is an empty string
    *
@@ -31,7 +42,7 @@ $ext(String.prototype, {
    * @return boolean check result
    */
   blank: function() {
-    return /^\s*$/.test(this);
+    return SPACES_ONLY_RE.test(this);
   },
   
   /**
@@ -40,9 +51,8 @@ $ext(String.prototype, {
    * @return String trimmed version
    */
   trim: String.prototype.trim || function() {
-    var str = this.replace(/^\s\s*/, ''),
-        ws = /\s/, i = str.length;
-    while (ws.test(str.charAt(--i)));
+    var str = this.replace(START_SPACES_RE, ''), i = str.length;
+    while (SOME_SPACE_RE.test(str.charAt(--i)));
     return str.slice(0, i + 1);
   },
   
@@ -51,7 +61,7 @@ $ext(String.prototype, {
    * @return String without tags
    */
   stripTags: function() {
-    return this.replace(/<\/?[^>]+>/ig, '');
+    return this.replace(STRIP_TAGS_RE, '');
   },
   
   /**
@@ -62,7 +72,7 @@ $ext(String.prototype, {
    */
   stripScripts: function(option) {
     var scripts = '';
-    var text = this.replace(/<script[^>]*>([\s\S]*?)<\/script>/img, function(match, source) {
+    var text = this.replace(STRIP_SCRIPTS_RE, function(match, source) {
       scripts += source.trim() + "\n";
       return '';
     });
@@ -103,10 +113,10 @@ $ext(String.prototype, {
    * @returns String camelized version
    */
   camelize: function() {
-    var prefix = this.match(/^(\-|_)+?/g) || ''; // <- keeps start dashes alive
+    var prefix = this.match(START_DASHES_RE) || ''; // <- keeps start dashes alive
     return prefix + this.substr(prefix.length, this.length).replace(
-       new RegExp('(\\-|_)+?(\\D)', 'g'), function(match) {
-         return match.replace(/\-|_/, '').toUpperCase();
+       DASH_PLUS_UPCASE_RE, function(match) {
+         return match.replace(SOME_DASH_RE, '').toUpperCase();
       });
   },
   
@@ -115,9 +125,9 @@ $ext(String.prototype, {
    * @return String underscored version
    */
   underscored: function() {
-    return this.replace(/([a-z0-9])([A-Z]+)/g, function(match, first, second) {
+    return this.replace(CASE_CHANGE_RE, function(match, first, second) {
       return first+"_"+(second.length > 1 ? second : second.toLowerCase());
-    }).replace(/\-/g, '_');
+    }).replace(ALL_DASHES_RE, '_');
   },
 
   /**
@@ -126,7 +136,7 @@ $ext(String.prototype, {
    * @return String captialised version
    */
   capitalize: function() {
-    return this.replace(new RegExp('(^|\\s|\\-|_)['+String.UTF8_DOWNS+']', 'g'), function(match) {
+    return this.replace(LOWCASED_WORD_START_RE, function(match) {
       return match.toUpperCase();
     });
   },
@@ -182,9 +192,9 @@ $ext(String.prototype, {
    * @return Float or NaN
    */
   toFloat: function(strict) {
-    return parseFloat(strict ? this : this.replace(',', '.').replace(/(\d)-(\d)/g, '$1.$2'));
+    return parseFloat(strict ? this : this.replace(',', '.').replace(FLOAT_DIGIT_RE, '$1.$2'));
   }
   
-});
+};})());
 
 $alias(String.prototype, {include: 'includes'});
