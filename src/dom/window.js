15b4107339459ad3cc5057e6d4f458c0cb0e89b5
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-23 18:04:19 +0400

    Getting rid of the _scrollTo hack

--- src/dom/window.js
+++ src/dom/window.js
@@ -3,49 +3,53 @@
  *
  * Copyright (C) 2008-2009 Nikolay V. Nemshilov aka St. <nemshilov#gma-il>
  */
-$ext(self, {
-  /**
-   * returns the inner-sizes of the window
-   *
-   * @return Object x: d+, y: d+
-   */
-  sizes: function() {
-    return this.innerWidth ? {x: this.innerWidth, y: this.innerHeight} :
-      {x: document.documentElement.clientWidth, y: document.documentElement.clientHeight};
-  },
+$ext(self, (function(win) {
+  var old_scroll = win.scrollTo;
   
-  /**
-   * returns the scrolls for the window
-   *
-   * @return Object x: d+, y: d+
-   */
-  scrolls: function() {
-    return (this.pageXOffset || this.pageYOffset) ? {x: this.pageXOffset, y: this.pageYOffset} :
-      (this.document.body.scrollLeft || this.document.body.scrollTop) ? 
-      {x: this.document.body.scrollLeft, y: this.document.body.scrollTop} :
-      {x: this.document.documentElement.scrollLeft, y: this.document.documentElement.scrollTop};
-  },
-  
-  /**
-   * overloading the native scrollTo method to support hashes and element references
-   *
-   * @param mixed number left position, a hash position, element or a string element id
-   * @param number top position
-   * @return window self
-   */
-  scrollTo: function(left, top) {
-    if(isElement(left) || (isString(left) && $(left))) {
-      left = $(left).position();
+return {
+    /**
+     * returns the inner-sizes of the window
+     *
+     * @return Object x: d+, y: d+
+     */
+    sizes: function() {
+      return this.innerWidth ? {x: this.innerWidth, y: this.innerHeight} :
+        {x: document.documentElement.clientWidth, y: document.documentElement.clientHeight};
+    },
+
+    /**
+     * returns the scrolls for the window
+     *
+     * @return Object x: d+, y: d+
+     */
+    scrolls: function() {
+      return (this.pageXOffset || this.pageYOffset) ? {x: this.pageXOffset, y: this.pageYOffset} :
+        (this.document.body.scrollLeft || this.document.body.scrollTop) ? 
+        {x: this.document.body.scrollLeft, y: this.document.body.scrollTop} :
+        {x: this.document.documentElement.scrollLeft, y: this.document.documentElement.scrollTop};
+    },
+
+    /**
+     * overloading the native scrollTo method to support hashes and element references
+     *
+     * @param mixed number left position, a hash position, element or a string element id
+     * @param number top position
+     * @return window self
+     */
+    scrollTo: function(left, top) {
+      if(isElement(left) || (isString(left) && $(left))) {
+        left = $(left).position();
+      }
+
+      if (isHash(left)) {
+        top  = left.y;
+        left = left.x;
+      }
+
+      old_scroll.call(this, left, top);
+
+      return this;
     }
-    
-    if (isHash(left)) {
-      top  = left.y;
-      left = left.x;
-    }
-    
-    this._scrollTo(left, top);
-    
-    return this;
-  },
-  _scrollTo: window.scrollTo
-});
+};
+
+})(window));
