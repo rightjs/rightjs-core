48d3a6879cb3143f53a0e1c194bd7fdbc8bfb40a
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-12-05 16:21:51 +0300

    Fx.Morph reworked it back to the user defined keys processing, case the automatical approach is lagging sometimes

--- src/fx/fx/morph.js
+++ src/fx/fx/morph.js
@@ -7,19 +7,51 @@
  *
  * Copyright (C) 2008-2009 Nikolay V. Nemshilov aka St. <nemshilov#gma-ilc-om>
  */
-Fx.Morph = new Class(Fx, {
+Fx.Morph = new Class(Fx, (function() {
+  // a list of common style names to compact the code a bit
+  var Top = 'Top', Left = 'Left', Right = 'Right', Bottom = 'Bottom', Color = 'Color', Style = 'Style',
+      Width = 'Width', Bg = 'background', Border = 'border', Pos = 'Position', BgColor = Bg + Color,
+      BdStyle = Border + Style, BdColor = Border + Color, BdWidth = Border + Width;
+  
+  
+  // adds variants to the style names list
+  var add_variants = function(keys, key, variants) {
+    for (var i=0; i < variants.length; i++)
+      keys.push(key + variants[i]);
+  };
+  
+  // parses the style hash into a processable format
+  var parse_style = function(values) {
+    var result = {}, re = /[\d\.\-]+/g, m;
+    
+    for (var key in values) {
+      m = values[key].match(re)
+      var value = m.map('toFloat');
+      value.t = values[key].split(re);
+      if (/^\d/.test(values[key]) && value.t[0] !== '') value.t.unshift('');
+      for (var i=0; i < value.length; i++) {
+        value.t.splice(i*2+1, 0, value[i]);
+      }
+      result[key] = value;
+    }
+    
+    return result;
+  };
+  
+return {
 
 // protected  
 
   // parepares the effect
   prepare: function(style) {
-    var before = this._cloneStyle(this.element),
-        after  = this._endStyle(style);
+    var keys   = this._styleKeys(style),
+        before = this._cloneStyle(this.element, keys),
+        after  = this._endStyle(style, keys);
     
     this._cleanStyles(before, after);
     
-    this.before = this._parse(before);
-    this.after  = this._parse(after);
+    this.before = parse_style(before);
+    this.after  = parse_style(after);
   },
   
   render: function(delta) {
@@ -38,63 +70,62 @@ Fx.Morph = new Class(Fx, {
   },
   
   /**
-   * Parses out the numericals out of the style values and nukes non-processables
-   *
-   * It kinda splits the values onto tokens and creates a list of numeric values
-   * that will be later processed inserted into the list of tokens and joined back
-   *
-   * @param Object raw styles
-   * @return Object parsed values
-   */
-  _parse: function(values) {
-    var result = {}, re = /[\d\.\-]+/g, m;
-    
-    for (var key in values) {
-      if (m = values[key].match(re)) {
-        var value = m.map('toFloat');
-        value.t = values[key].split(re);
-        if (/^\d/.test(values[key]) && value.t[0] !== '') value.t.unshift('');
-        for (var i=0; i < value.length; i++) {
-          value.t.splice(i*2+1, 0, value[i]);
-        }
-        result[key] = value;
-      }
-    }
-    
-    return result;
-  },
-    
-  /**
-   * fast clone for element's styles
-   * NOTE: don't do anything fancy in here, it supposed to work as fast a possible
-   *
-   * @param Element element
-   * @return Object style
-   */
-  _cloneStyle: function(element) {
-    var style = element.computedStyles(), clean = {}, style_keys = this.__keys;
-    for (var i=0, len = style_keys.length; i < len; i++)
-      clean[style_keys[i]] = style[style_keys[i]];
-    
-    return clean;
-  },
-  
-  /**
    * Returns a hash of the end style
    *
    * @param Object style
    * @return Object end style
    */
-  _endStyle: function(style) {
+  _endStyle: function(style, keys) {
     var parent = this.element.parentNode,
         dummy  = $(this.element.cloneNode(true)).setStyle(style);
         
     // swapping the element with the dummy and getting the new styles
     if (parent) parent.replaceChild(dummy, this.element);
-    var after  = this._cloneStyle(dummy);
+    var after  = this._cloneStyle(dummy, keys);
     if (parent) parent.replaceChild(this.element, dummy);
     
     return after;
+  },
+  
+  /**
+   * Fast styles cloning
+   *
+   * @param Element element
+   * @param Array style keys
+   * @return Hash of styles
+   */
+  _cloneStyle: function(element, keys) {
+    for (var i=0, len = keys.length, style = element.computedStyles(), clean = {}; i < len; i++)
+      clean[keys[i]] = style[keys[i]];
+    
+    return clean;
+  },
+  
+  /**
+   * creates an appropriate style-keys list out of the user styles
+   *
+   * @param Object the style hash
+   * @return Array of clean style keys list
+   */
+  _styleKeys: function(style) {
+    var keys = [], border_types = [Style, Color, Width], directions = [Top, Left, Right, Bottom];
+      
+    for (var key in style) {
+      if (key.startsWith(Border))
+        for (var i=0; i < border_types.length; i++)
+          for (var j=0; j < directions.length; j++)
+            keys.push(Border + directions[j] + border_types[i]);
+      else if (key === 'margin' || key === 'padding')
+        add_variants(keys, key, directions);
+      else if (key.startsWith(Bg))
+        add_variants(keys, Bg, [Color, Pos, Pos+'X', Pos+'Y']);
+      else if (key === 'opacity' && Browser.IE)
+        keys.push('filter');
+      else
+        keys.push(key);
+    }
+    
+    return keys;
   },
   
   /**
@@ -109,12 +140,12 @@ Fx.Morph = new Class(Fx, {
     
     for (var key in after) {
       // getting directional options together so they were processed faster
-      if (key.includes('Top')) {
+      if (key.includes(Top)) {
         var top = key,
-            left = key.replace('Top', 'Left'),
-            right = key.replace('Top', 'Right'),
-            bottom = key.replace('Top', 'Bottom'),
-            common = key.replace('Top', '');
+            left = key.replace(Top, Left),
+            right = key.replace(Top, Right),
+            bottom = key.replace(Top, Bottom),
+            common = key.replace(Top, '');
             
         if (after[top] === after[left] && after[top] === after[right] &&  after[top] === after[bottom] &&
             before[top] === before[left] && before[top] === before[right] && before[top] === before[bottom]
@@ -127,7 +158,7 @@ Fx.Morph = new Class(Fx, {
       }
       
       // checking the height/width options
-      if (key === 'width' || key === 'height') {
+      if (key === Width || key === 'height') {
         if (before[key] == 'auto') before[key] = this.element['offset'+key.capitalize()] + 'px';
       }
     }
@@ -136,13 +167,16 @@ Fx.Morph = new Class(Fx, {
     if (after.filter && !before.filter) before.filter = 'alpha(opacity=100)';
     
     // adjusting the border style
-    if (before.borderStyle != after.borderStyle) {
-      if (before.borderStyle == 'none') {
-        this.element.style.borderWidth =  '0px';
+    if (before[BdStyle] != after[BdStyle]) {
+      var style = this.element.style;
+      
+      if (before[BdStyle] == 'none') {
+        style[BdWidth] = '0px';
       }
-      this.element.style.borderStyle = after.borderStyle;
-      if (this._transp(before.borderColor)) {
-        this.element.style.borderColor = this.element.getStyle('color');
+      
+      style[BdStyle] = after[BdStyle];
+      if (this._transp(before[BdColor])) {
+        style[BdColor] = this.element.getStyle(Color);
       }
     }
     
@@ -164,6 +198,7 @@ Fx.Morph = new Class(Fx, {
       // filling up the missing sizes
       if (/\d/.test(after[key]) && !/\d/.test(before[key])) before[key] = after[key].replace(/[\d\.\-]+/g, '0');
       
+      // removing unprocessable keys
       if (after[key] === before[key] || remove.includes(key) || !/\d/.test(before[key]) || !/\d/.test(after[key])) {
         delete(after[key]);
         delete(before[key]);
@@ -174,7 +209,7 @@ Fx.Morph = new Class(Fx, {
   // looking for the visible background color of the element
   _getBGColor: function(element) {
     return [element].concat(element.parents()).map(function(node) {
-      var bg = node.getStyle('backgroundColor');
+      var bg = node.getStyle(BgColor);
       return (bg && !this._transp(bg)) ? bg : null; 
     }, this).compact().first() || '#FFF';
   },
@@ -185,31 +220,5 @@ Fx.Morph = new Class(Fx, {
     return color === 'transparent' || color === 'rgba(0, 0, 0, 0)';
   }
   
-});
-
-
-/**
- * This script prebuilds the style-keys list so that the visual effects initialization
- * was working faster
- */
-document.onReady(function() {
-  var e = $E('div').insertTo(document.body), style = e.computedStyles(), style_keys = [];
-  
-  if (style.length && style[0]) {
-    for (var i=0; i < style.length; i++) {
-      if (!style[i].startsWith('-')) {
-        style_keys.push(style[i].camelize());
-      }
-    }
-  } else {
-    for (var key in style) {
-      if (isString(style[key]) && !key.startsWith('Moz'))
-        style_keys.push(key);
-    }
-  }
-  
-  Fx.Morph.prototype.__keys = style_keys.filter('match', /colo|wid|hei|arg|add|pos|bor|top|lef|rig|bot|off|pas|opa|ilt|siz/i);
-  
-  e.remove();
-});
+}})());
 
