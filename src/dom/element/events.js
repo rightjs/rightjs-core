4ac30a1e45775eb7375b13c92f2150ba8e03f666
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-01-28 14:03:44 +0300

    Cleaner and compacter element-events hack

--- src/dom/element/events.js
+++ src/dom/element/events.js
@@ -6,7 +6,7 @@
 Element.include((function() {
   var observer = Observer.create({}, 
     $w('click rightclick contextmenu mousedown mouseup mouseover mouseout mousemove keypress keydown keyup')
-  );
+  ),
   
   //
   // HACK HACK HACK
@@ -14,38 +14,40 @@ Element.include((function() {
   // I'm kinda patching the observer methods manually in here
   // the reason is in building flat and fast functions
   //
-  observer.observe = observer.on = eval('['+
-    observer.observe.toString().replace(/(\$listeners\.push\((\w+?)\);)/, '$1'+
-      '$2.e=Event.cleanName($2.e);$2.n=Event.realName($2.e);'+
-      
-      '$2.w=function(){var a=$A(arguments),e=($2.r&&$2.r!=="stopEvent")?a.shift():Event.ext(a[0],this);'+
-        'return $2.f.apply(this,a.concat($2.a))};'+(
-      
-      self.attachEvent ?
-        '$2.w=$2.w.bind(this);this.attachEvent("on"+$2.n,$2.w);' :
-        'this.addEventListener($2.n,$2.w,false);'
-      )
-    )+
-  ']')[0];
+  hack = function(name, re, text) {
+    observer[name] = eval('['+ observer[name].toString().replace(re, text) +']')[0];
+  };
   
-  observer.stopObserving = eval('['+
-    observer.stopObserving.toString().replace(/(function\s*\((\w+)\)\s*\{\s*)(return\s*)([^}]+)/m, 
-      '$1var r=$4;'+
-      'if(!r)' + (self.attachEvent ? 
-        'this.detachEvent("on"+$2.n,$2.w);' :
-        'this.removeEventListener($2.n,$2.w,false);'
-      )+'$3 r')+
-  ']')[0];
+  hack('on', 
+    /(\$listeners\.push\((\w+?)\);)/,
+    
+    '$1$2.e=Event.cleanName($2.e);$2.n=Event.realName($2.e);'+
+    
+    '$2.w=function(){'+
+      'var a=$A(arguments),e=($2.r&&$2.r!=="stopEvent")?a.shift():Event.ext(a[0],this);'+
+      'return $2.f.apply(this,a.concat($2.a))};' + (
+        self.attachEvent ?
+          '$2.w=$2.w.bind(this);this.attachEvent("on"+$2.n,$2.w);' :
+          'this.addEventListener($2.n,$2.w,false);'
+        )
+  );
+  observer.observe = observer.on;
   
+  hack('stopObserving',
+    /(function\s*\((\w+)\)\s*\{\s*)(return\s*)([^}]+)/m, 
+    '$1var r=$4;'+
+    'if(!r)' + (self.attachEvent ? 
+      'this.detachEvent("on"+$2.n,$2.w);' :
+      'this.removeEventListener($2.n,$2.w,false);'
+    )+'$3 r'
+  );
   
-  observer.fire = eval('['+
-    observer.fire.toString().replace(/(\w+)\.f\.apply.*?\.concat\((\w+)\)[^}]/,
-      '$1.f.apply(this,[new Event($1.e,$2.shift())].concat($1.a).concat($2))'
-    )+
-  ']')[0];
+  hack('fire',
+    /(\w+)\.f\.apply.*?\.concat\((\w+)\)\)/,
+    '$1.f.apply(this,[new Event($1.e,$2.shift())].concat($1.a).concat($2))'
+  );
   
-  // a simple events terminator method to be hooked like
-  // this.onClick('stopEvent');
+  // a simple events terminator method to be hooked like this.onClick('stopEvent');
   observer.stopEvent = function(e) { e.stop(); };
   
   $ext(window,   observer);
