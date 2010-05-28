cf586b6a562fa399e46a10f05157b37cffe5bc99
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-05-28 10:21:03 +0400

    #246 a compacter olds loading script implementation

--- src/olds/loader.js
+++ src/olds/loader.js
@@ -10,11 +10,9 @@
  * Copyright (C) 2009-2010 Nikolay V. Nemshilov
  */
 if (!document.querySelector) {
-  (function() {
-    var rigth_src_re = /(^|\/)(right)([^\/]+)$/,
-        core_src = $A(document.getElementsByTagName('script')).map('src').compact().first('match', rigth_src_re);
-    if (core_src)
-      document.write('<scr'+'ipt src="'+core_src.replace(rigth_src_re, '$1$2-olds$3')+'"></scr'+'ipt>');
-  })();
+  document.write('<scr'+'ipt src="'+
+    $A(document.getElementsByTagName('script')).last().src
+      .replace(/(^|\/)(right)([^\/]+)$/, '$1$2-olds$3')+
+  '"></scr'+'ipt>');
 }
  
