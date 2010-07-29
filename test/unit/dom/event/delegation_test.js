6d0ffde3ac50065a74b2b533e36c1f191c81e0b8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-07-29 19:26:33 +0400

    #219 the new events delegation API

--- test/unit/dom/event/delegation_test.js
+++ test/unit/dom/event/delegation_test.js
@@ -7,175 +7,298 @@ var EventDelegationTest = TestCase.create({
   name: 'EventDelegationTest',
   
   setUp: function() {
-    var node1 = this.node1 = {
-      method: function() {
-        this.args = $A(arguments);
-      }
-    };
-    
-    var node2 = this.node2 = {
-      method: function() {
-        this.args = $A(arguments);
-      }
-    };
-    
-    this.base = {
-      select: function(rule) {
-        return rule == 'node1' ? [node1] : [node2];
-      }
-    };
+    this.el  = $E('div').insertTo(document.body);
+    this.el1 = $E('div', {id: 'first'}).insertTo(this.el);
+    this.el2 = $E('div', {id: 'second'}).insertTo(this.el);
+    this.el3 = $E('div', {id: 'third'}).insertTo(this.el);
   },
   
-  testDelegationByFunction: function() {
-    var delegation = Event.delegate({
-      node1: function() { this.value = 1; },
-      node2: function() { this.value = 2; }
-    });
+  tearDown: function() {
+    this.el.remove();
+  },
+  
+  testSimpleDelegation: function() {
+    var target1 = null;
+    var target2 = null;
     
-    this.assertInstanceOf(Function, delegation);
+    var event1 = null;
+    var event2 = null;
     
-    delegation.call(this.base, { target: this.node1 });
+    this.el.delegate('click', '#first',  function(e) { event1 = e; target1 = this; });
+    this.el.delegate('click', '#second', function(e) { event2 = e; target2 = this; });
     
-    this.assertEqual(1, this.node1.value);
-    this.assertFalse('value' in this.node2);
+    this.fireClick(this.el1._);
     
-    delete(this.node1.value);
+    this.assert(event1 instanceof Event);
+    this.assertSame(this.el1, target1);
     
-    delegation.call(this.base, { target: this.node2 });
+    this.assertNull(event2);
+    this.assertNull(target2);
     
-    this.assertFalse('value' in this.node1);
-    this.assertEqual(2, this.node2.value);
+    // trying to click the second element
+    target1 = target2 = event1 = event2 = null;
+    
+    this.fireClick(this.el2._);
+    
+    this.assert(event2 instanceof Event);
+    this.assertSame(this.el2, target2);
+    
+    this.assertNull(event1);
+    this.assertNull(target1);
+    
+    // trying to click the third the unhooked element
+    target1 = target2 = event1 = event2 = null;
+    
+    this.fireClick(this.el3._);
+    
+    this.assertNull(event1);
+    this.assertNull(target1);
+    
+    this.assertNull(event2);
+    this.assertNull(target2);
+  },
+  
+  testDelegationWithArguments: function() {
+    var event = null;
+    var args  = null;
+    
+    this.el.delegate('click', '#first', function(e) {
+      event = e; args = $A(arguments).slice(1)
+    }, 1, 2, 3);
+    
+    this.fireClick(this.el1._);
+    
+    this.assert(event instanceof Event);
+    this.assertEqual([1,2,3], args);
+  },
+  
+  testDelegationWithArray: function() {
+    var first_called = false;
+    var second_called = false;
+    
+    this.el.delegate('click', '#first', [
+      function() { first_called = true; },
+      function() { second_called = true; }
+    ]);
+    
+    this.fireClick(this.el1._);
+    
+    this.assert(first_called);
+    this.assert(second_called);
+  },
+  
+  testDelegationWithArrayAndArguments: function() {
+    var args1 = null;
+    var args2 = null;
+    
+    this.el.delegate('click', '#first', [
+      [function() { args1 = $A(arguments).slice(1); }, 1, 2, 3],
+      [function() { args2 = $A(arguments).slice(1); }, 4, 5, 6]
+    ]);
+    
+    this.fireClick(this.el1._);
+    
+    this.assertEqual([1,2,3], args1);
+    this.assertEqual([4,5,6], args2);
   },
   
   testDelegationByName: function() {
-    var delegation = Event.delegate({
-      node1: 'method',
-      node2: 'method'
-    });
+    var event1  = null;
+    var event2  = null;
+    var event3  = null;
+    var target1 = null;
+    var target2 = null;
+    var target3 = null;
     
-    delegation.call(this.base, { target: this.node1 });
+    this.el1.dummy = function(e) { target1 = this; event1 = e; };
+    this.el2.dummy = function(e) { target2 = this; event2 = e; };
+    this.el3.dummy = function(e) { target3 = this; event3 = e; };
     
-    this.assertEqual([], this.node1.args);
-    this.assertFalse('args' in this.node2);
+    this.el.delegate('click', '#first', 'dummy');
+    this.el.delegate('click', '#second', ['dummy']);
     
-    delete(this.node1.args);
+    this.fireClick(this.el1._);
+    this.fireClick(this.el2._);
     
-    delegation.call(this.base, { target: this.node2 });
+    // there is no events when called by name
+    this.assertNull(event1);
+    this.assertNull(event2);
     
-    this.assertFalse('args' in this.node1);
-    this.assertEqual([], this.node2.args);
+    this.assertSame(target1, this.el1);
+    this.assertSame(target2, this.el2);
+    
+    this.assertNull(event3);
+    this.assertNull(target3);
   },
   
-  testDelegationByNameWithArgs: function() {
-    var delegation = Event.delegate({
-      node1: ['method', 'arg1', 'arg2', 'arg3'],
-      node2: ['method', 'arg4', 'arg5', 'arg6']
-    });
+  testDelegationByNameWithArguments: function() {
+    var args1  = null;
+    var args2  = null;
+    var event1 = null;
+    var event2 = null;
     
-    delegation.call(this.base, { target: this.node1 });
+    this.el1.dummy = function() { args1 = $A(arguments) };
+    this.el2.dummy = function() { args2 = $A(arguments) };
     
-    this.assertEqual(['arg1', 'arg2', 'arg3'], this.node1.args);
-    this.assertFalse('args' in this.node2);
+    this.el.delegate('click', '#first', 'dummy', 1, 2, 3);
+    this.el.delegate('click', '#second', [['dummy', 4, 5, 6]]);
     
-    delete(this.node1.args);
+    this.fireClick(this.el1._);
+    this.fireClick(this.el2._);
     
-    delegation.call(this.base, { target: this.node2 });
+    // there is no events when called by name
+    this.assertNull(event1);
+    this.assertNull(event2);
     
-    this.assertFalse('args' in this.node1);
-    this.assertEqual(['arg4', 'arg5', 'arg6'], this.node2.args);
+    this.assertEqual([1,2,3], args1);
+    this.assertEqual([4,5,6], args2);
   },
   
-  testDocumentAttachment: function() {
-    var f = function() {}, c = function() {}, args;
+  testDelegationByHash: function() {
+    var args1   = null;
+    var args2   = null;
+    var args3   = null;
+    var event1  = null;
+    var event2  = null;
+    var event3  = null;
+    var target1 = null;
+    var target2 = null;
+    var target3 = null;
     
-    this.mock(Event, 'delegate', function() { args = $A(arguments); return f; });
+    this.el2.dummy = function() { args2 = $A(arguments) };
+    this.el3.dummy = function() { args3 = $A(arguments) };
     
-    var connections = Event.behave(".some.css.rule", 'click', c);
-    
-    this.assert($(document).observes('click', f));
-    this.assertEqual([{'.some.css.rule': [c]}], args);
-    
-    this.assertEqual({click: f}, connections);
-    
-    Event.behave(".some.css.rule", 'mouseover', 'hide');
-    
-    this.assert($(document).observes('mouseover', f));
-    this.assertEqual([{'.some.css.rule': ['hide']}], args);
-    
-    Event.behave(".some.css.rule", 'mouseout', 'addClass', 'some-class');
-    
-    this.assert($(document).observes('mouseover', f));
-    this.assertEqual([{'.some.css.rule': ['addClass', 'some-class']}], args);
-    
-    $(document)
-      .stopObserving('click', f)
-      .stopObserving('mouseout', f)
-      .stopObserving('mouseover', f)
-    
-    this.undoMock(Event, 'delegate');
-  },
-  
-  testDocumentAttachmentWithHash: function() {
-    var f = function() {}, c = function() {}, args = [];
-    
-    this.mock(Event, 'delegate', function() { args.push($A(arguments)); return f; });
-    
-    Event.behave(".some.css.rule", {
-      click: c,
-      mouseout: 'hide',
-      mouseover: ['addClass', 'some-class']
+    this.el.delegate('click', {
+      '#first':  function(e) { event1 = e; args1 = $A(arguments).slice(1); },
+      '#second': ['dummy', 1, 2, 3],
+      '#third':  'dummy'
     });
     
-    this.assert($(document).observes('click', f));
-    this.assertEqual([{'.some.css.rule': c}], args[0]);
+    this.fireClick(this.el1._);
+    this.fireClick(this.el2._);
+    this.fireClick(this.el3._);
     
-    this.assert($(document).observes('mouseout', f));
-    this.assertEqual([{'.some.css.rule': 'hide'}], args[1]);
+    this.assert(event1 instanceof Event);
     
-    this.assert($(document).observes('mouseover', f));
-    this.assertEqual([{'.some.css.rule': ['addClass', 'some-class']}], args[2]);
-    
-    $(document)
-      .stopObserving('click', f)
-      .stopObserving('mouseout', f)
-      .stopObserving('mouseover', f)
-    
-    this.undoMock(Event, 'delegate');
+    this.assertEqual([],      args1);
+    this.assertEqual([1,2,3], args2);
+    this.assertEqual([],      args3);
   },
   
-  testStoppingDelegation: function() {
-    var f = function() {}, c = function() {};
+  testDelegatesEmptyCase: function() {
+    var callback = function() {};
     
-    this.mock(Event, 'delegate', function() { return f; });
+    this.assertFalse(this.el.delegates('click'));
+    this.assertFalse(this.el.delegates('click', '#first'));
+    this.assertFalse(this.el.delegates('click', '#first', callback));
+  },
+  
+  testDelegatesPositiveCase: function() {
+    var callback = function() {};
     
-    var events = Event.behave(".some.css.rule", {
-      click: c,
-      mouseout: 'hide',
-      mouseover: ['addClass', 'some-class']
+    this.el.delegate('click', '#first', callback);
+    
+    this.assert(this.el.delegates('click'));
+    this.assert(this.el.delegates('click', '#first'));
+    this.assert(this.el.delegates('click', '#first', callback));
+    
+    // various false checks
+    this.assertFalse(this.el.delegates('mouseover'));
+    this.assertFalse(this.el.delegates('click', '#second'));
+    this.assertFalse(this.el.delegates('click', '#first', function() {}));
+  },
+  
+  testDelegatesInterference: function() {
+    this.el.onMouseover('addClass', 'boo');
+    
+    this.assertFalse(this.el.delegates('mouseover'));
+  },
+  
+  testDelegatesWithHash: function() {
+    var callback = function() {};
+    
+    this.el.delegate('click', '#first', callback);
+    
+    this.assert(this.el.delegates('click', {
+      '#first': callback
+    }));
+  },
+  
+  testUndelegate: function() {
+    var callback = function() {};
+    
+    this.el.on('click', callback);
+    this.el.delegate('click', '#first', callback);
+    this.el.delegate('mouseover', '#first', callback);
+    
+    this.assert(this.el.delegates('click'));
+    this.assert(this.el.observes('click'));
+    
+    this.el.undelegate('click');
+    
+    this.assertFalse(this.el.delegates('click'));
+    
+    this.assert(this.el.observes('click'));
+    this.assert(this.el.delegates('mouseover', '#first', callback));
+  },
+  
+  testUndelegateByCSSRule: function() {
+    this.el.delegate('click', '#first', 'boo');
+    this.el.delegate('click', '#second', 'boo');
+    
+    this.el.undelegate('click', '#first');
+    
+    this.assertFalse(this.el.delegates('click', '#first'));
+    this.assert(this.el.delegates('click', '#second'));
+  },
+  
+  testUndelegateByFunction: function() {
+    var callback1 = function() {};
+    var callback2 = function() {};
+    
+    this.el.delegate('click', '#first', [callback1, callback2]);
+    
+    this.el.undelegate('click', '#first', callback2);
+    
+    this.assert(this.el.delegates('click', '#first', callback1));
+    this.assertFalse(this.el.delegates('click', '#first', callback2));
+  },
+  
+  testUndelegateByName: function() {
+    this.el.delegate('click', '#first', ['method1', 'method2']);
+    
+    this.el.undelegate('click', '#first', 'method2');
+    
+    this.assert(this.el.delegates('click', '#first', 'method1'));
+    this.assertFalse(this.el.delegates('click', '#first', 'method2'));
+  },
+  
+  testUndelegateByHash: function() {
+    this.el.delegate('click', {
+      '#first':  'method1',
+      '#second': 'method2',
+      '#third':  'method3'
     });
     
-    this.assert($(document).observes('click',     f));
-    this.assert($(document).observes('mouseout',  f));
-    this.assert($(document).observes('mouseover', f));
+    this.el.undelegate('click', {
+      '#first':  'method1',
+      '#second': 'method2'
+    });
     
-    $(document).stopObserving(events);
-    
-    this.assertFalse($(document).observes('click', f));
-    this.assertFalse($(document).observes('mouseout', f));
-    this.assertFalse($(document).observes('mouseover', f));
-    
-    this.undoMock(Event, 'delegate');
+    this.assertFalse(this.el.delegates('click', '#first'));
+    this.assertFalse(this.el.delegates('click', '#second'));
+    this.assert(this.el.delegates('click', '#third'))
   },
   
   testStringOnShortcut: function() {
     var args;
-    this.mock(Event, 'behave', function() { args = $A(arguments); });
+    this.mock($(document), 'delegate', function() { args = $A(arguments); });
     
-    ".some.css-rule".on('addClass', 'foo');
+    ".some.css-rule".on('click', 'addClass', 'foo');
     
-    this.assertEqual(['.some.css-rule', 'addClass', 'foo'], args);
+    this.assertEqual(['click', '.some.css-rule', 'addClass', 'foo'], args);
     
-    this.undoMock(Event, 'behave');
+    this.undoMock($(document), 'delegate');
   },
   
   testStringOnNamedShortcuts: function() {
@@ -195,5 +318,4 @@ var EventDelegationTest = TestCase.create({
     
     String.prototype.on = old_method;
   }
-  
 });
