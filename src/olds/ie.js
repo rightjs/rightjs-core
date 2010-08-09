dc8a97c061e5148f1a56cd2094d6523c473ef02c
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-08-09 11:19:22 +0400

    IE6/7 search by ID fix

--- src/olds/ie.js
+++ src/olds/ie.js
@@ -3,17 +3,17 @@
  *
  *   Keep them in one place so they were more compact
  *
- * Copyright (C) 2009-2010 Nikolay V. Nemshilov
+ * Copyright (C) 2009-2010 Nikolay Nemshilov
  */
 if (RightJS.Browser.OLD) {
   // loads DOM element extensions for selected elements
   $ = RightJS.$ = (function(old_function) {
     return function(id) {
-      var element = old_function(id), match = RightJS.isString(id) ? /^#([\w\-]+)$/.exec(id) : null;
+      var element = old_function(id);
       
       // old IE browses match both, ID and NAME
-      if (element instanceof RightJS.Element && match !== null && element._.id !== match[1]) {
-        element = RightJS.$(document).first(id);
+      if (element && element instanceof RightJS.Element && RightJS.isString(id) && element._.id !== id) {
+        element = RightJS.$(document).first('#'+ id);
       }
         
       return element;
