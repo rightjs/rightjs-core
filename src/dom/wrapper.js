a1c4cd50735ce82559676e3f656772dc1d994af8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-10-09 16:41:04 +0400

    #366 exposing the dynamic typecasting interface

--- src/dom/wrapper.js
+++ src/dom/wrapper.js
@@ -14,12 +14,12 @@ var Wrapper = RightJS.Wrapper = function(parent, methods) {
   var Klass = function(object, options) {
     this.initialize(object, options);
 
-    var instance = this, unit = instance._, uid;
+    var instance = this, unit = instance._, uid, cast;
 
     // dynamically typecasting in case if the user is creating
     // an element of a subtype via the basic Element constructor
-    if (this.constructor === Element && unit.tagName in Element_wrappers) {
-      instance = new Element_wrappers[unit.tagName](unit);
+    if (this.constructor === Element && (cast = Wrapper.Cast(unit)) !== undefined) {
+      instance = new cast(unit);
       if ('$listeners' in this) {
         instance.$listeners = this.$listeners;
       }
@@ -44,6 +44,11 @@ var Wrapper = RightJS.Wrapper = function(parent, methods) {
 
   // including the basic tools
   return Klass.include({_: undefined}, methods);
+};
+
+// searches for a suitable class for dynamic typecasting
+Wrapper.Cast = function(unit) {
+  return Element_wrappers[unit.tagName];
 };
 
 // exposing the cache so it could be manupulated externally
